1061e59ef5596dfc4160be0f53857d53272a3315
Ryan Ly <ryanly.rl1@gmail.com>
2019-06-09 22:39:37 -0400

    Initial commit for mailing list API

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const PORT = process.env.PORT || 8080; // Use the process's port if given (PaaS), otherwise use 8080
 
 const app = express();
-const routes = require('src/routes');
+const routes = require('./routes/routes');
+const mailing_list = require('./routes/mailinglist')
 
 // Handle API endpoints
-app.use(routes);
+app.use('/', routes);
+app.use('/mailinglist/', mailing_list);
 
 // Start the server
 app.listen(PORT, function(){
