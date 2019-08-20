4a8ac1716e33917b2b2d43e57bb7ff19eabe46d5
Ryan Ly <ryanly@ciena.com>
2019-08-20 14:35:11 -0400

    Add support for HTTPS

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const PORT = process.env.PORT || 8080; // Use the process's port if given (PaaS), otherwise use 8080
 const passport = require('passport');
-const Authentication = require('./model/authentication')
+const Authentication = require('./model/authentication');
+const http = require('http');
+const https = require('https');
 
 const app = express();
 const routes = require('./routes/routes');
@@ -23,8 +25,18 @@ app.use('/', routes);
 app.use('/mailinglist/', mailing_list);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Start the server
-app.listen(PORT, function(){
-    console.log('Application server listening on port ' + PORT + " in " + process.env.NODE_ENV + " mode");
-    console.log('hello?'); 
-});
+// Use HTTPS for production
+if(process.env.NODE_ENV === "production"){
+    const options = {
+        key: fs.readFileSync(config.ssl_key),
+        cert: fs.readFileSync(config.ssl_cert)
+      };
+      
+    https.createServer(options, app).listen(8080)
+} else {
+    // Start the server
+    app.listen(PORT, function(){
+        console.log('Application server listening on port ' + PORT + " in " + process.env.NODE_ENV + " mode");
+        console.log('hello?'); 
+    });
+}
