ebc8b35eed209e6bc4b36e1226e41b7065a52f01
Ryan Ly <ryanly.rl1@gmail.com>
2019-12-21 21:34:38 -0500

    Remove filtering on application

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -493,7 +493,7 @@ UsersController.search = function(req, res) {
     let name = req.body.name;
     Database.getAll(COLLECTION_NAME).then(function(result){
         // Filter out users that don't have an application
-        let users = result.filter(user => user.application).map(user => {
+        let users = result.map(user => {
             return {
                 firstName: user.application.basicInfo.firstName,
                 lastName: user.application.basicInfo.lastName,
